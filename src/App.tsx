b403ed914dac19dee6dd38c20faa90e68ef167dd
Yoni Halperin <joftaz@gmail.com>
2026-01-30 21:46:18 +0200

    another fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ReviewDashboard from "./pages/ReviewDashboard";
 import UsagePolicy from "./pages/UsagePolicy";
+import Accessibility from "./pages/Accessibility";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/review" element={<ReviewDashboard />} />
           <Route path="/usage-policy" element={<UsagePolicy />} />
+          <Route path="/accessibility" element={<Accessibility />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
